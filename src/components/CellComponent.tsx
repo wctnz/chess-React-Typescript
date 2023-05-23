bc6ff355f6ce7494082cd9891fe613632977806b
«wctnz» <wctnzz@gmail.com>
2023-05-24 00:33:33 +0400

    second

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Cell } from '../models/Cell';
 
-const CellComponent = () => {
+interface CellProps {
+    cell: Cell;
+    selected: boolean; 
+    click: (cell: Cell) => void
+}
+
+const CellComponent: FC<CellProps> = ({ cell, selected, click }) => {
 return (
-<div className='cell'>
+<div className={["cell", cell.color, selected ? "selected" : ""].join(" ")}
+onClick={()=>click(cell)}
+style={{background: cell.available && cell.figure ? "green" : ""}}
+>
+{cell.available && !cell.figure && <div className={"available"}></div>}
+{ cell.figure?.logo && <img src={cell.figure.logo} alt=""/> }
 </div>
 );
 };
---
+++ src/models/figures/King.ts
@@ -0,0 +1,43 @@
+import { Cell } from './../Cell';
+import { Colors } from './../Colors';
+import { Figure, FigureNames } from "./Figure";
+import blackLogo from "../../assets/black-king.png"
+import whiteLogo from "../../assets/white-king.png"
+
+export class King extends Figure {
+
+    isFirstStep: boolean = true
+
+    constructor(color: Colors, cell: Cell) {
+        super(color, cell)
+        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo
+        this.name = FigureNames.KING
+    }
+    // canMove(target: Cell): boolean {
+    //     if(target.figure?.name === FigureNames.KING && target.figure?.color) console.log(target);
+    //     if(!super.canMove(target)) return false;
+    //     const isVerticalMove = (target.y === this.cell.y + 1 || target.y === this.cell.y - 1) && target.x === this.cell.x;
+    //     const isHorizontalMove = (target.x === this.cell.x + 1 || target.x === this.cell.x - 1) && target.y === this.cell.y;
+    //     const isLeftDiagonal = (
+    //       (target.x === this.cell.x + 1 && target.y === this.cell.y + 1) 
+    //       || (target.x === this.cell.x - 1 && target.y === this.cell.y - 1)
+    //     )
+    //     const isRightDiagonal = (
+    //       (target.x === this.cell.x + 1 && target.y === this.cell.y - 1) 
+    //       || (target.x === this.cell.x - 1 && target.y === this.cell.y + 1)
+    //     )
+
+    //     if(isVerticalMove 
+    //       || isHorizontalMove 
+    //       || isLeftDiagonal 
+    //       || isRightDiagonal) return true;
+
+    //     return false;
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target)) return false;
+        const dx = Math.abs(this.cell.x - target.x);
+        const dy = Math.abs(this.cell.y - target.y);
+        return dx <= 1 && dy <= 1;
+    }
+}
+
